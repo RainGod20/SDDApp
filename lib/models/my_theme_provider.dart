7cfc8a46395c793f6c049cb62583d74b883e8916
Varun Rao <varun.rao3@education.nsw.gov.au>
2023-12-27 11:04:10 +1100

    Changed the theme again based on current time

--- lib/models/my_theme_provider.dart
+++ lib/models/my_theme_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class MyThemeModel extends ChangeNotifier {
   // Is the time right now more than 6 am and less than 8 pm
-  bool _isLightTheme = DateTime.now().hour > 6 && DateTime.now().hour < 20 ? true : false;
+  bool _isLightTheme = DateTime.now().hour > 6 && DateTime.now().hour < 18 ? true : false;
 
   void changeTheme() {
     _isLightTheme = !_isLightTheme;
