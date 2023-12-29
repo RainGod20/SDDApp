1f9a64a8605197d05a057ffba5d115f24212f812
Varun Rao <varun.rao3@education.nsw.gov.au>
2023-12-30 09:47:16 +1100

    Made the screens have a separate body file

--- lib/screens/stopwatch_screen/stopwatch_screen.dart
+++ lib/screens/stopwatch_screen/stopwatch_screen.dart
@@ -1,9 +1,6 @@
 // ignore_for_file: deprecated_member_use
-
-import 'dart:async';
-
-import 'package:clock_app_flutter/constants.dart';
 import 'package:clock_app_flutter/models/my_theme_provider.dart';
+import 'package:clock_app_flutter/screens/stopwatch_screen/stopwatch_body.dart';
 import 'package:clock_app_flutter/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -17,181 +14,12 @@ class StopwatchScreen extends StatefulWidget {
 }
 
 class _StopwatchScreenState extends State<StopwatchScreen> {
-  int seconds = 0, minutes = 0, hours = 0;
-  String digitSeconds = "00", digitMinutes = "00", digitHours = "00";
-  Timer? timer;
-  bool started = false;
-  List laps = [];
-
-  // Stop Timer Function
-  void stop() {
-    timer!.cancel();
-    setState(() {
-      started = false;
-    });
-  }
-
-  // Reset Timer Functions
-  void reset() {
-    timer!.cancel();
-    setState(() {
-      laps.clear();
-
-      seconds = 0;
-      minutes = 0;
-      hours = 0;
-
-      digitSeconds = "00";
-      digitMinutes = "00";
-      digitHours = "00";
-
-      started = false;
-    });
-  }
-
-  void addLaps() {
-    String lap = "$digitHours:$digitMinutes:$digitSeconds";
-    setState(() {
-      laps.add(lap);
-    });
-  }
-
-  // Start Timer Function
-  void start() {
-    started = true;
-    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      int localSeconds = seconds + 1;
-      int localMinutes = minutes;
-      int localHours = hours;
-
-      if (localSeconds > 59) {
-        if (localMinutes > 59) {
-          localHours++;
-          localMinutes = 0;
-        } else {
-          localMinutes++;
-          localSeconds = 0;
-        }
-      }
-
-      setState(() {
-        seconds = localSeconds;
-        minutes = localMinutes;
-        hours = localHours;
-
-        digitSeconds = (seconds >= 10) ? "$seconds" : "0$seconds";
-        digitMinutes = (minutes >= 10) ? "$minutes" : "0$minutes";
-        digitHours = (hours >= 10) ? "$hours" : "0$hours";
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: buildDrawer(context),
       appBar: buildAppBar(context),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 20.0,
-            ),
-            Center(
-              child: Text(
-                "$digitHours:$digitMinutes:$digitSeconds",
-                style: Theme.of(context).textTheme.displayMedium,
-              ),
-            ),
-            Container(
-              height: 400,
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.secondary,
-                borderRadius: BorderRadius.circular(20),
-                border: Border.all(color: Theme.of(context).colorScheme.onSecondary),
-                boxShadow: Theme.of(context).colorScheme.background == Colors.white
-                    ? [
-                        BoxShadow(
-                          color: kShadowColor.withOpacity(0.14),
-                          blurRadius: 64,
-                          spreadRadius: 64,
-                        )
-                      ]
-                    : null,
-              ),
-              child: ListView.builder(
-                itemCount: laps.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Lap Number ${index + 1}",
-                          style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                        Text(
-                          "${laps[index]}",
-                          style: Theme.of(context).textTheme.titleMedium,
-                        )
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: RawMaterialButton(
-                    onPressed: () {
-                      (!started) ? start() : stop();
-                    },
-                    shape: StadiumBorder(
-                      side: BorderSide(color: Theme.of(context).primaryColor),
-                    ),
-                    child: Text(
-                      (!started) ? "Start" : "Pause",
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 8),
-                IconButton(
-                  color: Theme.of(context).colorScheme.tertiary,
-                  onPressed: () {
-                    addLaps();
-                  },
-                  icon: const Icon(Icons.flag),
-                ),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: RawMaterialButton(
-                    onPressed: () {
-                      reset();
-                    },
-                    fillColor: Theme.of(context).primaryColor,
-                    shape:
-                        StadiumBorder(side: BorderSide(color: Theme.of(context).primaryColor)),
-                    child: const Text(
-                      "Reset",
-                      style: TextStyle(color: kTitleTextLightColor, fontSize: 15),
-                    ),
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+      body: const StopwatchBody(),
     );
   }
 
---
+++ lib/screens/timer_screen/timer_body.dart
@@ -0,0 +1,201 @@
+import 'package:clock_app_flutter/constants.dart';
+import 'package:clock_app_flutter/models/my_theme_provider.dart';
+import 'package:clock_app_flutter/screens/timer_screen/round_button.dart';
+import 'package:clock_app_flutter/size_config.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
+
+class TimerBody extends StatefulWidget {
+  const TimerBody({super.key});
+
+  @override
+  State<TimerBody> createState() => _TimerBodyState();
+}
+
+class _TimerBodyState extends State<TimerBody> with TickerProviderStateMixin {
+  late AnimationController controller;
+  bool isPlaying = false;
+
+  String get countText {
+    Duration count = controller.duration! * controller.value;
+    return controller.isDismissed
+        ? '${controller.duration!.inHours}:${(controller.duration!.inMinutes % 60).toString().padLeft(2, '0')}:${(controller.duration!.inSeconds % 60).toString().padLeft(2, '0')}'
+        : '${count.inHours}:${(count.inMinutes % 60).toString().padLeft(2, '0')}:${(count.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
+
+  double progressValue = 0;
+  Duration lastDuration = const Duration(seconds: 60);
+
+  Future<void> notify() async {
+    if (countText == '0:00:00') {
+      FlutterRingtonePlayer().play(
+        fromAsset: "assets/icons/ringtone.mp3",
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 60),
+    );
+    controller.addListener(
+      () {
+        notify();
+        if (controller.isDismissed) {
+          setState(() {
+            progressValue = 1.0;
+            isPlaying = false;
+          });
+        } else {
+          setState(
+            () {
+              progressValue = controller.value;
+            },
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Center(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: getProportionateScreenWidth(325),
+                  height: getProportionateScreenWidth(325),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.surface,
+                    shape: BoxShape.circle,
+                    boxShadow: Theme.of(context).colorScheme.background == Colors.white
+                        ? [
+                            BoxShadow(
+                              color: kShadowColor.withOpacity(0.14),
+                              blurRadius: 64,
+                              spreadRadius: 64,
+                            )
+                          ]
+                        : null,
+                  ),
+                  child: CircularProgressIndicator(
+                    backgroundColor: Theme.of(context).colorScheme.onSecondary,
+                    color: Theme.of(context).primaryColor,
+                    strokeWidth: 6,
+                    value: progressValue,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if (controller.isDismissed) {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) => Container(
+                          color: Colors.white,
+                          height: 300,
+                          child: CupertinoTimerPicker(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.background == Colors.white
+                                    ? Colors.white
+                                    : Colors.white70,
+                            initialTimerDuration: lastDuration,
+                            onTimerDurationChanged: (time) {
+                              setState(
+                                () {
+                                  controller.duration = time;
+                                  lastDuration = time;
+                                },
+                              );
+                            },
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: AnimatedBuilder(
+                    animation: controller,
+                    builder: (context, child) => Text(
+                      countText,
+                      style: Theme.of(context).textTheme.displayMedium,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 50,
+                  left: 0,
+                  right: 0,
+                  child: Consumer<MyThemeModel>(
+                    builder: (context, theme, child) => GestureDetector(
+                      onTap: () => theme.changeTheme(),
+                      child: SvgPicture.asset(
+                        theme.isLightTheme ? "assets/icons/Sun.svg" : "assets/icons/Moon.svg",
+                        height: 24,
+                        width: 24,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  if (controller.isAnimating == false) {
+                    controller.reverse(from: controller.value == 0 ? 1.0 : controller.value);
+                    setState(() {
+                      isPlaying = true;
+                    });
+                  } else {
+                    controller.stop();
+                    setState(
+                      () {
+                        isPlaying = false;
+                      },
+                    );
+                  }
+                },
+                child: RoundButton(
+                  icon: isPlaying ? (Icons.pause) : Icons.play_arrow,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  controller.reset();
+                  setState(() {
+                    isPlaying = false;
+                  });
+                },
+                child: const RoundButton(
+                  icon: Icons.stop,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
